e329ffd57cf1720c45be3b185aa4a13cb0af9adc
Kirsten Ziman <kirstenkmbziman@gmail.com>
2017-08-24 13:17:39 -0400

    added catch trial (needs fixing)

--- posner2.py
+++ posner2.py
@@ -3,14 +3,10 @@ import random
 import os
 
 vers = '2.0'
-#modified 1.3
-#removed gaussian probe, replaced with text/images on either side
-
 
 #clocks
 globalClock = core.Clock()
 logging.setDefaultClock(globalClock)
-
 
 #experiment parameters and participant info
 
@@ -28,6 +24,8 @@ if not dlg.OK:
 info['fixFrames'] = 60
 info['cueFrames'] = 60
 info['probeFrames'] = 60
+info['catchProbeFrames'] = 30
+info['catchProbePause'] = 30
 info['dateStr'] = data.getDateStr()
 
 #filenames
@@ -96,90 +94,167 @@ respClock = core.Clock()
 def runBlock(loop = object, saveData = True):
     """Runs a loop for an experimental block and saves reposnes if requested"""
     for thisTrial in loop:
-        #generate csv file
-        #filename = "data/" + info['participant'] + "_" + info['dateStr'] + "conds" + trial#
-            #populate csv with desired params
-            #add number of the trial to the filename
-            #CHRISTINA
+        trialType = random.choice([1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+        if trialType == 2: #run image trial
+            
+            #generate csv file
+            #filename = "data/" + info['participant'] + "_" + info['dateStr'] + "conds" + trial#
+                #populate csv with desired params
+                #add number of the trial to the filename
+                #CHRISTINA
+            
+            img1_file = random.choice(os.listdir("/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Faces"))
+            img1 = '/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Faces/'+img1_file
+            
+            img2_file = random.choice(os.listdir("/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Objects"))
+            img2 = '/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Objects/'+img2_file
+            
+            probe1 = visual.ImageStim(win, img1, size=probeSize) #pos=(5, 0), size=probeSize)
+            probe2 = visual.ImageStim(win, img2, size=probeSize) #pos=(-5, 0), size=probeSize)
+            
+            resp = None
+            rt = None
+            
+            probe1.setPos( [thisTrial['probeX'], 0] )
+            probe2.setPos( [-thisTrial['probeX'], 0] )
+            
+            #trandomize side of cue
+            #random boolean --> if True, cue right
+            if bool(random.getrandbits(1)) == True:
+                cue.setPos(5)
+            else:
+                cue.setPos(-5)
+            
+            fixation.setAutoDraw(True)
+            for frameN in range(info['fixFrames']):
+                win.flip()
         
+            cue.setAutoDraw(True)
+            for frameN in range(info['cueFrames']):
+                win.flip()
+            cue.setAutoDraw(False)
+            
+            probe1.setAutoDraw(True)
+            probe2.setAutoDraw(True)
+            win.callOnFlip(respClock.reset)
+            event.clearEvents()
+            for frameN in range(info['probeFrames']):
+                if frameN == 0:
+                    respClock.reset()
+                keys = event.getKeys(keyList = ['left','right','escape'])
+                if len(keys) > 0:
+                    resp = keys[0]
+                    rt = respClock.getTime()
+                    break
+                win.flip()
+            probe1.setAutoDraw(False)
+            probe2.setAutoDraw(False)
+            fixation.setAutoDraw(False)
         
-        img1_file = random.choice(os.listdir("/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Faces"))
-        img1 = '/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Faces/'+img1_file
-        
-        img2_file = random.choice(os.listdir("/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Objects"))
-        img2 = '/Users/kirstenziman/Documents/GitHub/P4N2016/OddballLocStims/Objects/'+img2_file
-        
-        probe1 = visual.ImageStim(win, img1, size=probeSize) #pos=(5, 0), size=probeSize)
-        probe2 = visual.ImageStim(win, img2, size=probeSize) #pos=(-5, 0), size=probeSize)
-        
-        resp = None
-        rt = None
-        
-        probe1.setPos( [thisTrial['probeX'], 0] )
-        probe2.setPos( [-thisTrial['probeX'], 0] )
-        
-        #trandomize side of cue
-        #random boolean --> if True, cue right
-        if bool(random.getrandbits(1)) == True:
-            cue.setPos(5)
-        else:
-            cue.setPos(-5)
-        
-        fixation.setAutoDraw(True)
-        for frameN in range(info['fixFrames']):
+            #clear screen
             win.flip()
-    
-        cue.setAutoDraw(True)
-        for frameN in range(info['cueFrames']):
-            win.flip()
-        cue.setAutoDraw(False)
         
-        probe1.setAutoDraw(True)
-        probe2.setAutoDraw(True)
-        win.callOnFlip(respClock.reset)
-        event.clearEvents()
-        for frameN in range(info['probeFrames']):
-            if frameN == 0:
-                respClock.reset()
-            keys = event.getKeys(keyList = ['left','right','escape'])
-            if len(keys) > 0:
+    #        if resp == None:
+    #            
+    #            keys = event.waitKeys(keyList = ['left','right','escape'])
+    #            resp = keys[0]
+    #            rt = respClock.getTime()
+            
+            if saveData == True:
+                if thisTrial['probeX']>0 and resp=='right':
+                    corr = 1
+                elif thisTrial['probeX']<0 and resp=='left':
+                    corr = 1
+                elif resp=='escape':
+                    corr = None
+                    trials.finished = True
+                else:
+                    corr = 0
+            
+                trials.addData('resp', resp)
+                trials.addData('rt', rt)
+                trials.addData('corr', corr)
+                thisExp.nextEntry()
+        else: #run catch trial
+            resp = None
+            rt = None
+            
+            #cue = visual.Circle(win, size = cueSize, lineColor = 'white', fillColor = 'lightGrey')
+            probe = visual.Circle(win, size = cueSize, lineColor = 'white', fillColor = 'lightGrey')
+            position = random.choice([-8,8])
+            
+            probe.setPos( [position, 0] )
+            
+            #trandomize side of cue
+            #random boolean --> if True, cue right
+            if bool(random.getrandbits(1)) == True:
+                cue.setPos(5)
+            else:
+                cue.setPos(-5)
+            
+            fixation.setAutoDraw(True)
+            for frameN in range(info['fixFrames']):
+                win.flip()
+        
+            cue.setAutoDraw(True)
+            for frameN in range(info['cueFrames']):
+                win.flip()
+            cue.setAutoDraw(False)
+            
+            #probe.setAutoDraw(True)
+            win.callOnFlip(respClock.reset)
+            event.clearEvents()
+            for frameN in range(info['catchProbeFrames']):
+                probe.setAutoDraw(True)
+                if frameN == 0:
+                    respClock.reset()
+                keys = event.getKeys(keyList = ['left','right','escape'])
+                if len(keys) > 0:
+                    resp = keys[0]
+                    rt = respClock.getTime()
+                    break
+
+            win.flip() #clear screen
+            
+            for frameN in range(info['catchProbePause']):
+                probe.setAutoDraw(False)
+  
+                if len(keys) > 0:
+                    resp = keys[0]
+                    rt = respClock.getTime()
+                    break
+                    
+                win.flip()
+            probe.setAutoDraw(False)
+            fixation.setAutoDraw(False)
+        
+            #clear screen
+            win.flip()
+        
+            if resp == None:
+                
+                keys = event.waitKeys(keyList = ['left','right','escape'])
                 resp = keys[0]
                 rt = respClock.getTime()
-                break
-            win.flip()
-        probe1.setAutoDraw(False)
-        probe2.setAutoDraw(False)
-        fixation.setAutoDraw(False)
-    
-        #clear screen
-        win.flip()
-    
-#        if resp == None:
-#            
-#            keys = event.waitKeys(keyList = ['left','right','escape'])
-#            resp = keys[0]
-#            rt = respClock.getTime()
-        
-        if saveData == True:
-            if thisTrial['probeX']>0 and resp=='right':
-                corr = 1
-            elif thisTrial['probeX']<0 and resp=='left':
-                corr = 1
-            elif resp=='escape':
-                corr = None
-                trials.finished = True
-            else:
-                corr = 0
-        
-            trials.addData('resp', resp)
-            trials.addData('rt', rt)
-            trials.addData('corr', corr)
-            thisExp.nextEntry()
-
-
+            
+            if saveData == True: 
+                if thisTrial['probeX']>0 and resp=='right':
+                    corr = 1
+                elif thisTrial['probeX']<0 and resp=='left':
+                    corr = 1
+                elif resp=='escape':
+                    corr = None
+                    trials.finished = True
+                else:
+                    corr = 0
+            
+                trials.addData('resp', resp)
+                trials.addData('rt', rt)
+                trials.addData('corr', corr)
+                thisExp.nextEntry()
 #show instructions and run practice and experimental trials
-showInstructions(text = instructPractice, acceptedKeys = ['return', 'escape'])
-runBlock(practice, saveData = False)
+#showInstructions(text = instructPractice, acceptedKeys = ['return', 'escape'])
+#runBlock(practice, saveData = False)
 showInstructions(text = instructExp, acceptedKeys = ['return', 'escape'])
 runBlock(trials)
 showInstructions(text = instructThanks, acceptedKeys = ['return'])
