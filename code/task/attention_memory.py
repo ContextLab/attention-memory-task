2a185d2507906e628a40f32d2c01bfbbd9b3d9c3
Kirsten Ziman <kirstenkmbziman@gmail.com>
2018-06-21 13:36:16 -0400

    practice trials up to cue; need to debug dynamic practice trials

--- code/task/attention_memory.py
+++ code/task/attention_memory.py
@@ -34,15 +34,14 @@ paths = {'data_path':'../../data/', 'stim_path':'../../stim/'}
 
 # Obtain participant info (pop-up) and make subdir #
 info = subject_info(experiment_title, paths['data_path'])
-subject_directory = subject_directory(info, paths['data_path'], path_only=True)
-paths['subject'] = subject_directory
+paths['subject'] = subject_directory(info, paths['data_path'], path_only=True)
 
 # Initiate clock #
 global_clock = core.Clock()
 logging.setDefaultClock(global_clock)
 
 # pre questionnaire #
-pre_info = pre_questionnaire(info, save=save_data, save_path=subject_directory)
+pre_info = pre_questionnaire(info, save=save_data, save_path=paths['subject'])
 
 # Window and Stimulus timing #
 win = visual.Window([1024,768], fullscr = True, monitor = 'testMonitor', units='deg', color = 'black')
@@ -54,31 +53,27 @@ timing = {'cue':int(round(1.5 * rate)), 'probe':int(round(3.0 * rate)), 'mem':in
 # Run Experiment #####################################################################################
 
 
-
-
 # Initiate log file #
 # log_data = logging.LogFile(paths['data_path'] + subject_info['participant'] + '.log', filemode='w', level=logging.DATA)
 
 
-# Window #
-
-
-
 # Instructions & Practice #
-#if practice:
-    # run intro & pract rounds
+if practice:
+    for x in range(11):
+        practice_instructions(win, paths, pract_text(x), x, timing, acceptedKeys = [])
 
 
 # eye tracker callibration #
 # if eye_track:
-    
+
     
 # MRI sync #
 # if MRI:
 
 
 # Initialize subject dataframe #
-df = initialize_df(info, categories, paths, subject_directory, params) 
+df = initialize_df(info, categories, paths, paths['subject'], params) 
+
 
 # create df masks
 mask1 = df['Trial Type']=='Presentation'
@@ -87,15 +82,18 @@ mask2 = df['Trial Type']=='Memory'
 # Pres & Mem runs #
 for run in range(params['runs']):
     
-    # show pres instruct (0 or other) 
-    
     mask3 = df['Run']==run
+    
+    # presentation      
+    text_present(win, pres_text(run))
     presentation_run(win, df.loc[mask1][mask3], params, timing, paths) 
     
-    # show memory instruct (0 or other)
-    
+    # memory 
+    text_present(win, mem_text(run))
     memory_run(win, df.loc[mask2][mask3], params, timing, paths)
     
+# thanks for participating 
+show_instructions(win, paths, thank_text(), acceptedKeys = [])
 
 # post questionnaire #
 #post_info = post_questionnaire(subject[0], save=save_data, save_path=paths['data_path'])
