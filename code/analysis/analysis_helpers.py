b38ad821c3e8c12f3f8a0e3b360a5c1a34e48c28
student <Student@ebbinghaus.kiewit.dartmouth.edu>
2018-07-06 23:50:24 -0400

    notebook updates and sample data

--- code/analysis/analysis_helpers.py
+++ code/analysis/analysis_helpers.py
@@ -63,14 +63,15 @@ def validity_check(df, params):
     '''
     num_valid = sum(list(df['Cue Validity']))
 
+    msg = []
+
     if len(df.Run.unique())<params['runs']:
         msg = ["It looks like there is test data here! (Fewer than expected # of runs).  "]
 
-    if num_valid != params['presentations_per_run']*params['runs']*(100-params['invalid_cue_percentage'])/100:
-        if len(df.Run.unique())==params['runs']:
-            msg.append('Incorrect number of invalid attention circles.  ')
-    else:
-        msg = []
+    if len(msg)==0:
+        if num_valid != params['presentations_per_run']*params['runs']*(100-params['invalid_cue_percentage'])/100:
+            if len(df.Run.unique())==params['runs']:
+                msg.append('Incorrect number of invalid attention circles.  ')
 
     return(msg)
 
