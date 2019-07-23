88fb18b4d60a751e7654ad45bb4965aefd7cfd7a
Kirsten Ziman <kirstenkmbziman@gmail.com>
2019-07-23 00:38:08 -0400

    exp1 notebooks clean(er); parsed data stored separate from raw data files

--- Experiment1_BlockCue/code/analysis/analysis_helpers.py
+++ Experiment1_BlockCue/code/analysis/analysis_helpers.py
@@ -32,7 +32,7 @@ def sum_pd(subdir):
 
     return(df)
 
-# Functions for Simple Behavioral Analyses
+
 
 def add_level(df):
     '''
@@ -45,11 +45,14 @@ def add_level(df):
 
     return(df)
 
+
+
 def run_level(df):
     '''
     input: df containing pres and mem from single run
     output: df with string in 'Attention Level' column in each Memory trial row
     '''
+
     cued_cat = df[df['Trial Type']=='Presentation']['Cued Category'].tolist()[0]
 
     for index,row in df.iterrows():
@@ -72,52 +75,262 @@ def run_level(df):
                             elif cue == 'Cued ':
                                 attention = "Side"
                         df['Attention Level'][index] = attention
+                        df['Cued Category'][index] = cued_cat
 
     mem_mask = df['Trial Type']=='Memory'
     df.loc[mem_mask,'Attention Level'] = df.loc[mem_mask,'Attention Level'].fillna('Novel')
 
     return(df)
 
-def ROC(df, plot=True):
+
+# Functions for Simple Behavioral Analyses
+
+def success_prop(df):
+    '''
+    input:   subject's dataframe
+    output:  proportion of correct attention probe responses
+    '''
+    success = df[(df['Attention Probe']=='x') & (df['Attention Button']==1.0)].shape[0] + df[(df['Attention Probe']=='o') & (df['Attention Button']==3.0)].shape[0]
+    fail    = df[(df['Attention Probe']=='x') & (df['Attention Button']==3.0)].shape[0] + df[(df['Attention Probe']=='o') & (df['Attention Button']==1.0)].shape[0]
+    prop    = float(success / (success + fail))
+    return(prop)
+
+
+
+def attn_success(dataframe, runs=False):
+    '''
+    input:  behavioral data, full experiment (pandas dataframe)
+    output: if runs==True -> run-wise proportions for each subject (list of lists)
+            if runs!=True -> averaged proportion correct for each subject (list of floats)
+    '''
+
+    prop = []
+    for sub in dataframe['Subject'].unique():
+
+        if runs == False:
+            df = dataframe[dataframe['Subject']==sub]
+            prop.append(success_prop(df))
+
+        else:
+            sub_props=[]
+            for run in range(0,8):
+                df = dataframe[(dataframe['Subject']==sub) & (dataframe['Run']==run)]
+                sub_props.append(success_prop(df))
+            prop.append(sub_props)
+
+    return(prop)
+
+
+
+def ROC_prop(df, rate, attn, novel='matched'):
     '''
     input: subject df
-    output: ROC plot or ROC data dict
+           rating - Familiarity score (float between 1.0 and 4.0)
+           attn - at time of encoding (string)
+
+    output: proportion of images encoded at ATTENTION LEVEL
+            given a score of RATE or higher, sorted by
+            CATEGORY (if category == True)
     '''
-    ratings = [1.0, 2.0, 3.0, 4.0]
-    ROC = {}
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
+    # proportions
+    combined = float(df.loc[(df['Attention Level'] == attn) & (df['Familiarity Rating'] >= rate)].shape[0])/(df.loc[(df['Attention Level'] == attn) & (df['Familiarity Rating'] > 0)].shape[0])
 
-    # for each attention level
-    for attn in ['Novel', 'None','Side','Full','Category']:
-
-        ROC[attn] = [0, 1]
-
-        # for each possible number rating
-        for rate in ratings:
-
-            # proportion of images in that attn level rated this rating or higher
-            num = df.loc[(df['Attention Level'] == attn) & (df['Familiarity Rating'] >= rate)].shape[0]
-            denom = df.loc[df['Attention Level'] == attn].shape[0]
-            ROC[attn].append(float(num)/denom)
-
-        ROC[attn].sort()
-
-        # proportions of various attention-level images, by rating, on y-axis
-        # proportions of novel images, by rating, on x-axis
-        if attn != 'Novel':
-            ax1.plot(ROC['Novel'], ROC[attn], '-o', label=attn)
-
-    if plot:
-        plt.legend(loc='upper left');
-        plt.ylim(0, 1)
-        plt.xlim(0, 1)
-        plt.gca().set_aspect('equal', adjustable='box')
-        plt.show()
+    if novel == 'all':
+        # all novel images for desired ratings in both categories
+        denom_f = df.loc[(df['Attention Level'] == attn) & (df['Familiarity Rating'] > 0)].shape[0]
+        denom_p = denom_f
 
     else:
-        return(ROC)
+        if novel   == 'matched':
+            f_nov,p_nov = 'Face','Place'
+        elif novel == 'opposite':
+            p_nov,f_nov = 'Place','Face'
+
+        denom_p = df.loc[(df['Attention Level'] == attn) & (df['Category'] == p_nov) & (df['Familiarity Rating'] > 0)].shape[0]
+        denom_f = (df.loc[(df['Attention Level'] == attn) & (df['Category'] == f_nov) & (df['Familiarity Rating'] > 0)].shape[0])
+
+    if df.loc[(df['Attention Level'] == attn) & (df['Category'] == 'Face')].shape[0] > 0:
+        face = float(df.loc[(df['Attention Level'] == attn) & (df['Familiarity Rating'] >= rate) & (df['Category'] == 'Face')].shape[0])/denom_f
+    else:
+        face = np.nan
+
+    if df.loc[(df['Attention Level'] == attn) & (df['Category'] == 'Place')].shape[0] > 0:
+        house = float(df.loc[(df['Attention Level'] == attn) & (df['Familiarity Rating'] >= rate) & (df['Category'] == 'Place')].shape[0])/denom_p
+    else:
+        house = np.nan
+
+    props = [combined, face, house]
+    return(props)
+
+
+
+def ROC_data(df, novel='matched'):
+    '''
+    input: subject df
+    output: list of three ROC proportion sets (list of dicts): 1) all images, 2) faces, 3) places
+    '''
+
+    ratings = [4.0, 3.0, 2.0, 1.0]
+    ROC, ROC_f, ROC_h = {},{},{}
+
+    # for each attention level
+    for attn in ['Novel', 'None', 'Side', 'Full', 'Category']:
+        for idx,roc in enumerate([ROC, ROC_f, ROC_h]):
+            roc[attn] = [0]
+            #roc[attn] = []
+
+            # for each possible number rating
+            for rate in ratings:
+                roc[attn].append(ROC_prop(df, rate, attn, novel=novel)[idx])
+
+    return(ROC, ROC_f, ROC_h)
+
+
+
+def ROC_plot(ROC_data):
+    '''
+    input: ROC proportions (dictionary)
+    output: displays plot of ROC curve
+    '''
+
+    fig, ax = plt.subplots()
+
+    for attn,color in zip(['Category', 'Full', 'None', 'Side'],['purple','blue','orange','red']):
+        ax.plot(ROC_data['Novel'], ROC_data[attn], '-o', label=attn, color=color)
+
+    plt.legend(loc='upper left');
+    plt.ylim(0, 1)
+    plt.xlim(0, 1)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
+
+
+def ROC_df(df):
+
+    '''
+    input:  full df
+    output: list of subject df's with run-wise ROC data
+    '''
+
+    subject_list = []
+
+    for sub in df['Subject'].unique():
+        subject=[]
+
+        for run in df['Run'].unique():
+            subject.append(ROC_data(df[(df['Run']==run) & (df['Subject']==sub)]))
+        subject_list.append(subject)
+
+    return(subject_list)
+
+
+
+def AUC(x_vals, y_vals):
+    '''
+    input:
+    x_vals - x values for ROC curve, arranged left to right (list of floats)
+    y_vals - y values for ROC curve, index matched to x_vals (list of floats)
+
+    output:
+    Area under the curve (AUC)
+    '''
+
+    AUC = 0
+
+    for i,(x,y) in enumerate(zip(x_vals, y_vals)):
+        if i>0:
+            x_delt = x - x_vals[i-1]
+            y_delt = y - y_vals[i-1]
+            AUC += (y_vals[i-1] * x_delt) + (.5 * x_delt * y_delt)
+
+    return(AUC)
+
+def slide_window_plot(data, window_len, overlap):
+
+    # re-label novel face and novel place images in full data
+    # change to 'NovelF' and 'NovelP'
+
+    data.loc[(data['Attention Level']=='Novel')
+             &(data['Category']=='Face'),'Attention Level'] = 'Novel_F'
+
+    data.loc[(data['Attention Level']=='Novel')
+             &(data['Category']=='Place'),'Attention Level'] = 'Novel_P'
+
+    all_subjects = {}
+
+    # within each subject
+    for s in data['Subject'].unique():
+
+        sub_runs = {}
+        # dictionary for windows in this subject's runs
+
+        face_nov_idx = data[(data['Category']=='Face') &
+                      (data['Trial Type']=='Memory') &
+                      (data['Attention Level']=='Novel') &
+                      (data['Subject']==s)].index
+
+        place_nov_idx = data[(data['Category']=='Face') &
+                       (data['Trial Type']=='Memory') &
+                       (data['Attention Level']=='Novel') &
+                       (data['Subject']==s)].index
+
+        # within each run
+        for r in data['Run'].unique():
+
+            run_ratios = []
+
+            # obtain cued category from last presentation in the pres block
+            cat = data[(data['Subject'] == s) &
+                        (data['Run'] == r)]['Cued Category']
+
+            # grab cued cateogry from last presentation trial
+            cued_letter = cat.iloc[-1][0]
+
+            if cued_letter == 'F':
+                uncued_letter = 'P'
+
+            if cued_letter == 'P':
+                uncued_letter = 'F'
+
+            # get chunk of Attn Levels + Ratings
+            level = data[(data['Subject'] == s) & (data['Run'] == r)]['Attention Level']
+            rating = data[(data['Subject'] == s) & (data['Run'] == r)]['Familiarity Rating']
+
+            novel_face_index = level[level=='Novel_F'].index
+            novel_place_index = level[level=='Novel_P'].index
+            memory_index = data[(data['Subject'] == s) & (data['Run'] == r)& (data['Trial Type']=='Memory')].index
+
+            windows = sliding_window(memory_index, window_len, overlap)
+
+            for window in windows:
+
+                face_win  = list(set(novel_face_index) & set(window))
+                place_win = list(set(novel_place_index) & set(window))
+
+                if cued_letter == 'F':
+                    ratio = rating[face_win].mean()/rating[place_win].mean()
+                else:
+                    ratio = rating[place_win].mean()/rating[face_win].mean()
+
+                run_ratios.append(ratio)
+                # list of cued / uncued ratios for this run (one per window)
+
+            sub_runs['run'+str(r)] = run_ratios
+
+            all_subjects['subj_'+str(s)] = pd.DataFrame.from_dict(sub_runs).T.mean()
+
+    melted = pd.melt(pd.DataFrame(all_subjects).T)
+
+    ax = sns.lineplot(x="variable", y="value", data=melted)
+
+    ax.set(xlabel='Time Window', ylabel='Mean Cued / Uncued Familiarity')
+    plt.show()
+
+    # print('Ratio of mean familiarity score for cued category novel images over uncued cateogry novel images, over time.')
+    print('Sliding window length: '+ str(window_len)+' with overlap of '+str(overlap)+'. Confidence interval over subject means.')
+    print('Total number of trials : 40 ')
 
 
 # EYE GAZE DATA ANALYSIS FUNCTIONS
@@ -178,37 +391,93 @@ def df_create(data):
 
     return(df)
 
-def pres_gaze(subdir, eye_df, interval='images'):
+
+def pres_gaze_image(subdir, eye_df, subject):
     '''
     input: subject's experiment df and eye track df
-    output: list of eye data df's
-            each df is either eye data from full pres block, or from single pres trial (interval='images')
+    output: list of eye tracking df's, one for each presentation trial
     '''
 
-    pres_gaze = []
+    pres_gaze = {'0':[],'1':[],'2':[],'3':[],'4':[],'5':[],'6':[],'7':[]}
 
     for f in os.listdir(subdir):
-
         if 'pres' in f:
 
+            num = f[-5]
             pres_df = pd.read_csv(subdir+'/'+f)
+            start = pres_df['Stimulus Onset']
+            end = pres_df['Stimulus End'] #+pres_df['Attention Reaction Time (s)'][9]
+            eye_df['Run']=num
+            eye_df['Subject'] = subject
 
-            if interval == 'images':
-                start = pres_df['Stimulus Onset']
-                end = pres_df['Stimulus End']
-
-            else:
-                start = pres_df['Stimulus Onset'][0]
-                end = pres_df['Stimulus End'][9]
-
-            for x,y in zip(start,end):
-                eye_df['Cued Side']=pres_df.iloc[0]['Cued Side']
-                eye_df['Cued Category']=pres_df.iloc[0]['Cued Category']
-                pres_gaze.append(eye_df.loc[(eye_df['timestamp']>x) &
-                                            (eye_df['timestamp']<y) &
+            trial = 0
+            for on,off in zip(start, end):
+                eye_df['Trial']=trial
+                pres_gaze[str(num)].append(eye_df.loc[(eye_df['timestamp']>on) &
+                                            (eye_df['timestamp']<off) &
                                             (eye_df['xRaw_righteye']>0.0) &
                                             (eye_df['xRaw_lefteye']>0.0)])
+                trial+=1
+
     return(pres_gaze)
+
+
+# def pres_gaze(subdir, eye_df, interval='images'):
+#     '''
+#     input: subject's experiment df and eye track df
+#     output: list of eye data df's
+#             each df is either eye data from full pres block, or from single pres trial (interval='images')
+#     '''
+#
+#     pres_gaze = []
+#
+#     for f in os.listdir(subdir):
+#
+#         stim=[]
+#
+#         if 'pres' in f:
+#
+#             pres_df = pd.read_csv(subdir+'/'+f)
+#
+#             if interval == 'images':
+#                 start = pres_df['Stimulus Onset']
+#                 end = pres_df['Stimulus End']
+#                 cued_f = pres_df['Cued Face']
+#                 cued_p = pres_df['Cued Place']
+#                 uncued_f = pres_df['Uncued Face']
+#                 uncued_p = pres_df['Uncued Place']
+#
+#
+#             else:
+#                 start = pres_df['Stimulus Onset'][0]
+#                 end = pres_df['Stimulus End'][9]
+#
+#             for x,y,cf,cp,uf,up in zip(start,end,cued_f,cued_p,uncued_f,uncued_p):
+#                 eye_df['Cued Side']=pres_df.iloc[0]['Cued Side']
+#                 eye_df['Cued Category']=pres_df.iloc[0]['Cued Category']
+#
+#                 eye_df['Cued Face']=cf
+#                 eye_df['Cued Place']=cp
+#                 eye_df['Uncued Face']=uf
+#                 eye_df['Uncued Place']=up
+#
+#
+#                 # eye_df.loc[(eye_df['timestamp']>x) &
+#                 #            (eye_df['timestamp']<y) &
+#                 #            (eye_df['xRaw_righteye']>0.0) &
+#                 #            (eye_df['xRaw_lefteye']>0.0)]['Image']=
+#
+#                 pres_gaze.append(eye_df.loc[(eye_df['timestamp']>x)&
+#                                             (eye_df['timestamp']<y)&
+#                                             (eye_df['xRaw_righteye']>0.0) &
+#                                             (eye_df['xRaw_lefteye']>0.0)&
+#                                             (eye_df['av_x_coord']<59.8)&
+#                                             (eye_df['yRaw_lefteye']>0.0)&
+#                                             (eye_df['yRaw_righteye']>0.0)&
+#                                             (eye_df['av_y_coord']<33.6)])
+#                 #stim.append()
+#     return(pres_gaze)
+
 
 
 def gaze_plot(df_list):
@@ -245,3 +514,68 @@ def gaze_plot(df_list):
     plt.ylim(0, 33.6)
     plt.xlim(0, 59.8)
     plt.show()
+
+    return(fig)
+
+
+
+def gaze_box(trial, center, dims):
+    '''
+    inputs:  center - tuple indicating center of ROI (x,y)
+             dims - tuple indicating dimensions of rectangle (width, height)
+             trial - dataframe for single presentation trial (or block)
+
+    outputs: box_num - raw number of gazepoints within box (int)
+             proportion - proportion of this trial's gaze points within box (float)
+             df - dataframe of all gazepoints within the box, including timestamps
+    '''
+
+    total_num = trial.shape[0]
+
+    trial = trial[(trial['av_y_coord']>=(center[1]-dims[1]/2))&
+                  (trial['av_y_coord']<=(center[1]+dims[1]/2))&
+                  (trial['av_x_coord']>=(center[0]-dims[0]/2))&
+                  (trial['av_x_coord']<=(center[0]+dims[0]/2))]
+
+    df = trial
+
+    if total_num>0:
+        box_num = trial.shape[0]
+        proportion = box_num/total_num
+    else:
+        box_num = np.nan
+        proportion = np.nan
+
+    return(box_num, proportion, df)
+
+def eye_intial(path):
+    ''' reads in raw eye gaze data
+        outputs eye gaze dataframe, timestamps in GMT
+    '''
+
+    data = []
+
+    for x in os.listdir(path):
+        newFile = parseFile(path+x)
+        data1 = newFile.parse()
+
+        for a,b in zip(['true','false'], ['True', 'False']):
+            data1 = data1.replace(a, b)
+
+        data1 = data1.split('\n')
+        data1 = [x for x in data1 if "tracker" in x]
+        data.extend(data1)
+
+    dict_list = [ast.literal_eval(x) for x in data]
+    dict_list = [x['values']['frame'] for x in dict_list if 'values' in x and 'frame' in x['values']]
+    df = pd.DataFrame(dict_list)
+
+    for eye in ['righteye','lefteye']:
+        for coord in ['x','y']:
+            df[coord+'Raw_'+eye] = [df[eye][row]['raw'][coord] for row in df.index.values]
+
+    df['av_x_coord'] = df[['xRaw_righteye', 'xRaw_lefteye']].mean(axis=1)
+    df['av_y_coord'] = df[['yRaw_righteye', 'yRaw_lefteye']].mean(axis=1)
+    df['timestamp']=[time.mktime(time.strptime(x[:], "%Y-%m-%d %H:%M:%S.%f")) for x in df['timestamp']]
+
+    return(df)
